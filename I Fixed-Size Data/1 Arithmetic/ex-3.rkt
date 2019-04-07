5d4531a73b7a490a9a2e17aa8bc39b40e60fd52f
ignat <buyfng@gmail.com>
2019-04-07 10:07:45 +0300

    Discard optional args

--- I Fixed-Size Data/1 Arithmetic/ex-3.rkt
+++ I Fixed-Size Data/1 Arithmetic/ex-3.rkt
@@ -6,4 +6,4 @@
 
 (string-append (substring str 0 i)
                "_"
-               (substring str i (string-length str)))
+               (substring str i))
--- I Fixed-Size Data/1 Arithmetic/ex-4.rkt
+++ I Fixed-Size Data/1 Arithmetic/ex-4.rkt
@@ -2,10 +2,10 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-4) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define str "helloworld")
-(define i 11)
+(define i 5)
 
 (string-append (substring str 0 (sub1 i))
-               (substring str i (string-length str)))
+               (substring str i))
 
 ; for i, values that between 1 and number of characters
 ; in str are legitimate
