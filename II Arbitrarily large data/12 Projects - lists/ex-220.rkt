dc73b3131576ac4952efddef9c225cf0061596b8
ignat <ignat@ignat.co>
2026-06-13 13:56:16 +0200

    Start simple tetris implementation

---
+++ II Arbitrarily large data/12 Projects - lists/ex-220.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex-220) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define WIDTH 10) ; # of blocks, horizontally
+(define SIZE 10) ; blocks are squares
+(define SCENE-SIZE (* WIDTH SIZE))
+
+(define BLOCK ; red squares with black rims
+  (overlay (square (- SIZE 1) "solid" "red")
+           (square (SIZE "outline" "black"))))
