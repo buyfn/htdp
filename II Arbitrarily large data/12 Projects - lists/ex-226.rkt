3e2fa032741646de42dd7bfebcdabc00b7479dd6
ignat <ignat@ignat.co>
2026-06-29 14:09:53 +0200

    solve ex. 226

---
+++ II Arbitrarily large data/12 Projects - lists/ex-226.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname ex-226) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp")) #f)))
+
+; An FSM is one of:
+; - '()
+; - (cons Transition FSM)
+
+(define-struct transition [current next])
+; A Transition is a structure:
+; (make-transition FSM-State FSM-State)
+
+; FSM-State is a Color
+
+; interpretation An FSM represents the transitions that a
+; finite state machine can take from one state to another
+; in reaction to keystrokes
+
+; FSM-State FSM-State -> Boolean
+; determines whether two states are equal
+(check-expect (state=? "blue" "green") #false)
+(check-expect (state=? "green" "green") #true)
+(define (state=? s1 s2)
+  (cond
+    [(and (image-color? s1)
+          (image-color? s2))
+     (equal? s1 s2)]
+    [else (error "state=?: at least one of the arguments is not a Color")]))
