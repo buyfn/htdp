2af359d93bca2f518f613c1876c6d15a68063f77
ignat <ignat@ignat.co>
2026-07-05 19:14:59 +0200

    solve ex. 251

---
+++ III Abstraction/15 Designing abstractions/ex-251.rkt
@@ -0,0 +1,24 @@
+#lang htdp/isl
+
+; [X Y -> Y] T [List-of X] -> Y
+; aggregates all the values from l into
+; a single value using f pairwise
+(define (fold1 f initial-value l)
+  (cond
+    [(empty? l) initial-value]
+    [else (f (first l)
+             (fold1 f initial-value (rest l)))]))
+
+; [List-of Number] -> Number
+; computes the sum of the numbers in l
+(check-expect (sum '()) 0)
+(check-expect (sum '(1)) 1)
+(check-expect (sum '(1 2 3)) 6)
+(define (sum l) (fold1 + 0 l))
+
+; [List-of Number] -> Number
+; computes the product of the numbers in l
+(check-expect (product '()) 1)
+(check-expect (product '(2)) 2)
+(check-expect (product '(2 3 4)) 24)
+(define (product l) (fold1 * 1 l))
