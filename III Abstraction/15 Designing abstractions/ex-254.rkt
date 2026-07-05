abb9be2c1b7ecb41e2d68da81b4ee2c8f8cb18dd
ignat <ignat@ignat.co>
2026-07-05 23:08:00 +0200

    solve ex. 254, 255

---
+++ III Abstraction/15 Designing abstractions/ex-254.rkt
@@ -0,0 +1,9 @@
+#lang htdp/isl
+
+; [List-of Number] [Number Number -> Boolean] -> [List-of Number]
+(define (sort-n lon compare) lon)
+
+; [List-of String] [String String -> Boolean] -> [List-of String]
+(define (sort-s los compare) los)
+
+; [List-of T] [T T -> Boolean] -> [List-of T]
---
+++ III Abstraction/15 Designing abstractions/ex-255.rkt
@@ -0,0 +1,9 @@
+#lang htdp/isl
+
+; [List-of Number] [Number -> Number] -> [List-of Number]
+(define (map-n lon f) lon)
+
+; [List-of String] [String -> String] -> [List-of String]
+(define (map-s los f) los)
+
+; [List-of X] [X -> Y] -> [List-of Y]
