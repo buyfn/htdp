369e43e485730a1412b96739a866c31248ee8aa6
ignat <ignat@ignat.co>
2026-07-12 23:00:01 +0200

    solve ex. 271

---
+++ III Abstraction/16 Using abstractions/ex-271.rkt
@@ -0,0 +1,40 @@
+#lang htdp/isl+
+
+; String [List-of String] -> Boolean
+; determines whether any name in lon is equal or
+; an extension of name
+(check-expect (find-name "Ann" '()) #false)
+(check-expect (find-name "Ann" (list "Ann")) #true)
+(check-expect (find-name "Ann" (list "Anna")) #true)
+(check-expect (find-name "Ann" (list "Bob" "Carl")) #false)
+(check-expect (find-name "Anna" (list "Ann")) #false)
+(check-expect (find-name "Ann" (list "Joanna")) #false)
+(define (find-name name lon)
+  (local ((define name-length (string-length name))
+          ; String -> Boolean
+          ; determines whether s is equal or extension of name
+          (define (equal-or-extension? s)
+            (cond
+              [(< (string-length s) name-length) #false]
+              [else (string=? (substring s 0 name-length) name)])))
+    (ormap equal-or-extension? lon)))
+
+; [List-of String] -> Boolean
+; determines whether all strings in lon start with "a"
+(check-expect (starts-with-a*? '()) #true)
+(check-expect (starts-with-a*? (list "apple" "ant")) #true)
+(check-expect (starts-with-a*? (list "apple" "bee")) #false)
+(check-expect (starts-with-a*? (list "")) #false)
+(check-expect (starts-with-a*? (list "Apple")) #false)
+(define (starts-with-a*? lon)
+  (local (; String -> Boolean
+          ; determines whether s starts with "a"
+          (define (starts-with-a? s)
+            (cond
+              [(= (string-length s) 0) #false]
+              [else (string=? (string-ith s 0) "a")])))
+    (andmap starts-with-a? lon)))
+
+; Either ormap or andmap could be used to implement
+; a function that ensures that no name on some list exceeds a given width.
+; Using andmap fits the problem statement better though.
