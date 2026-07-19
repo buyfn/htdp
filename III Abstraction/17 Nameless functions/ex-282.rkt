a3b784d2ded3cadf2084295ef60653bd6cb93d11
ignat <ignat@ignat.co>
2026-07-19 11:59:17 +0200

    solve ex. 282-285

---
+++ III Abstraction/17 Nameless functions/ex-282.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname ex-282) (read-case-sensitive #true) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#true constructor repeating-decimal #false #true none #false ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "itunes.rkt" "teachpack" "2htdp") (lib "web-io.rkt" "teachpack" "2htdp")) #false)))
+; Number -> Boolean
+(define (compare x)
+  (= (f-plain x) (f-lambda x)))
+
+(define (f-plain x)
+  (* 10 x))
+
+(define f-lambda
+  (lambda (x)
+    (* 10 x)))
+
+(compare (random 100000))
