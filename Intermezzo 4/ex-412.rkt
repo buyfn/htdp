ef3130c999d30a975fa93dd7a0e8c8a60a4d9786
ignat <ignat@ignat.co>
2026-09-09 20:35:31 +0200

    solve ex. 412

--- Intermezzo 4/ex-412.rkt
+++ Intermezzo 4/ex-412.rkt
@@ -26,4 +26,43 @@
                  (inex-exponent an-inex)))))
 
 (define MAX-POSITIVE (create-inex 99 1 99))
-(define MAX-NEGATIVE (create-inex 99 -1 99))
+(define MIN-POSITIVE (create-inex 1 -1 99))
+
+; Inex Inex -> Inex
+; adds two Inex numbers together
+; signals an error, if the result is out of range
+(check-expect (inex+ (create-inex 55 1 0)
+                     (create-inex 55 1 0))
+              (create-inex 11 1 1))
+(check-expect (inex+ (create-inex 55 -1 0)
+                     (create-inex 55 -1 0))
+              (create-inex 11 1 1))
+(check-expect (inex+ (create-inex 56 1 0)
+                     (create-inex 56 1 0))
+              (create-inex 11 1 1))
+(check-expect (inex+ (create-inex 55 -1 1)
+                     (create-inex 55 -1 1))
+              (create-inex 11 1 0))
+(define (inex+ a b)
+  (local ((define mantissa-sum
+            (+ (inex-mantissa a)
+               (inex-mantissa b)))
+          (define mantissa-overflow? (> mantissa-sum 99))
+          (define mantissa-sum-normalized
+            (if mantissa-overflow?
+                (round (/ mantissa-sum 10))
+                mantissa-sum))
+          (define signed-exponent
+            (* (inex-sign a) (inex-exponent a)))
+          (define new-signed-exponent
+            (if mantissa-overflow?
+                (+ signed-exponent 1)
+                signed-exponent))
+          (define new-sign
+            (if (>= new-signed-exponent 0) 1 -1))
+          (define new-exponent (abs new-signed-exponent)))
+    (if (> new-exponent 99)
+        (error "sum out of bounds")
+        (create-inex mantissa-sum-normalized
+                     new-sign
+                     new-exponent))))
